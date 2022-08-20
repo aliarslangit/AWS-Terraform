356da8bcce4153d2fdc3479305a5977b63be7147
Ali Arslan <aliarslanmushtaqali@gmail.com>
2022-08-20 14:07:05 +0500

    Update providers.tf

--- AWS EC2/providers.tf
+++ AWS EC2/providers.tf
@@ -9,6 +9,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  # access_key = var.aws_access_key
+  # secret_key = var.aws_secret_key
 }
